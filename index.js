67561dced859308759f23b8daa30e6f24e5794c9
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2023-07-03 14:24:06 -0500

    first commit

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ DB.once("open", () => {
 app.use(morgan("tiny"))
 app.use(
   cors({
-    origin: ["https://64a31e7ea3ef6d47ec7df1d9--quiet-mermaid-f1173b.netlify.app/"],
-    methods: ["GET", "POST"],
-    credentials: true,
+    origin: "https://64a31e7ea3ef6d47ec7df1d9--quiet-mermaid-f1173b.netlify.app/",
+    // methods: ["GET", "POST"],
+    // credentials: true,
   })
 );
 
